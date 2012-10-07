ae78dac6d254aa1f2bffbf804993bbafb2c080d1
Georgy Krasulya <gkrasulya@gmail.com>
2012-10-07 15:20:23 +0400

    fixed pages/region

--- pages/region.php
+++ pages/region.php
@@ -1,20 +1,15 @@
 <?
+
 $sql = "
-	SELECT h.*
-	FROM hotels h
+	SELECT DISTINCT(h.id), h.*
+	FROM hotels h, hotels_regions as hr
 	WHERE
-		region='$r'
-		AND (active=1 OR type='admin' OR type='')
-	$s LIMIT $start, $x";
-// $s - ordering
+		h.id = hr.hotel_id AND hr.region_id = {$r} $s
+	LIMIT $start, $x
+";
 
 $result = mysql_query($sql);
 
-
-	// WHERE 
-if (! $result) {
-	echo mysql_error();
-}
 if (mysql_num_rows($result) > 0) {
 	$result_r = mysql_query("SELECT title,country FROM regions WHERE id='$r'",$db);
 	$myrow_r = mysql_fetch_array($result_r);
@@ -38,7 +33,6 @@ if (mysql_num_rows($result) > 0) {
 	do {
 		$result_img = mysql_query("SELECT img_pre FROM fotos WHERE id='$myrow[foto]'",$db);
 		$myrow_img = mysql_fetch_array($result_img);
-		$photo = get_record("SELECT * FROM fotos WHERE id='$myrow[foto]'");
 		if ($myrow['rooms'] != 0) { $show_rooms = "<span class='add'>номеров: <strong>".$myrow['rooms']."</strong></span>"; } else { $show_rooms = ''; }
 		?>
 		
@@ -46,7 +40,7 @@ if (mysql_num_rows($result) > 0) {
 			<tr>
 				<td valign='top' width='200'>
 					<a href='/show/<?=$myrow['slug']?>.html' class='img'>
-						<img src='<?= SITE_ADDR ?>image.php?image=<?= SITE_ADDR ?>fotos/<?=$photo->img_big?>&amp;width=<?= PRE_IMG_WIDTH ?>' alt='<?=$myrow['title']?>' /></a>
+						<img src='/fotos/<?=$myrow_img['img_pre']?>' alt='<?=$myrow['title']?>' />
 					</a>
 				</td>
 				<td>
@@ -57,7 +51,7 @@ if (mysql_num_rows($result) > 0) {
 						
 						<span class='add'>цена: <strong><?=$myrow['price']?></strong></span>
 					</p>
-					<div class='text'><?=$myrow['descr_html'] ? $myrow['descr_html'] : $myrow['descr']?></div>
+					<p class='text'><?=$myrow['descr']?></p>
 					<span class='links'>
 						<a href='/show/<?=$myrow['slug']?>.html'>подробнее</a> <span>/</span>
 						<a href='/?form=new&amp;number=<?=$myrow['number']?>'>сделать заявку</a>
